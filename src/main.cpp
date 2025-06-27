e901e2764fd0bf5668a41cace636215d8f50e318
op30mmd <mamadop30@gmail.com>
2025-06-27 18:00:45 +0330

    fix workflow

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,19 @@
 
 int main(int argc, char *argv[])
 {
-        QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
     // Enable High-DPI scaling
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
 
-    QApplication::setStyle("windows");
+    // Set the style based on the platform
+    QString style = QStyleFactory::create("Fusion") ? "Fusion" : "Windows";
+    if (QStyleFactory::keys().contains("windowsvista")) {
+        style = "windowsvista";  // Use Windows Vista style if available (modern Windows look)
+    }
+    QApplication::setStyle(style);
 
     // This now works because the constructor is back to the default
     MainWindow w;
